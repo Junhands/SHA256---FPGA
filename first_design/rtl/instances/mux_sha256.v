9e5c3cd787c02011efbc040a2be8c678fd406987
JunHands <22520169@gm.uit.edu.vn>
2025-05-12 09:37:50 +0700

    delete 2 old design

--- first_design/rtl/instances/mux_sha256.v
+++
@@ -1,9 +0,0 @@
-`default_nettype none
-module mux_sha256(
-    input  wire [255:0] H0_constants,
-    input  wire [255:0] digest_of_block,
-    input  wire         sel,
-    output wire [255:0] H0_init
-);
-    assign H0_init = (ready) ? digest_of_block : H0_constants;
-endmodule
